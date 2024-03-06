fdab2c7cae8c985db83e92ecdf5f830b1dc81d76
Dalso <flskwhdhk@gmail.com>
2024-03-06 20:09:55 +0900

    feat : 코멘트 남기는 페이지 완성

--- lib/presentation/ui/detail_anime/detail_anime_item/user_comment/comment_write_screen.dart
+++ lib/presentation/ui/detail_anime/detail_anime_item/user_comment/comment_write_screen.dart
@@ -30,27 +30,39 @@ class CommentWriteScreen extends StatelessWidget {
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
-                  color: Theme.of(context).dividerColor,
+                  color: state.rating == 0
+                      ? Theme.of(context).dividerColor
+                      : Theme.of(context).shadowColor,
                 ),
               ),
-              Text('별점을 남겨주세요',
+              Container(
+                height: 30,
+                padding: const EdgeInsets.all(4.0),
+                child: Text(
+                  ratingLabel(rate: state.rating),
                   style: TextStyle(
                     color: Theme.of(context).dividerColor,
-                  )),
-              RatingBar(
-                allowHalfRating: true,
-                itemCount: 5,
-                ratingWidget: RatingWidget(
-                  full: Icon(Icons.star, color: Theme.of(context).primaryColor),
-                  half: Icon(Icons.star_half,
-                      color: Theme.of(context).primaryColor),
-                  empty: Icon(Icons.star,
-                      color: Theme.of(context).dividerColor.withOpacity(0.3)),
+                  ),
                 ),
-                itemSize: 40,
-                onRatingUpdate: (double value) {
-                  viewModel.onUpdateRating(rating: value);
-                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RatingBar(
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  ratingWidget: RatingWidget(
+                    full:
+                        Icon(Icons.star, color: Theme.of(context).primaryColor),
+                    half: Icon(Icons.star_half,
+                        color: Theme.of(context).primaryColor),
+                    empty: Icon(Icons.star,
+                        color: Theme.of(context).dividerColor.withOpacity(0.3)),
+                  ),
+                  itemSize: 40,
+                  onRatingUpdate: (double value) {
+                    viewModel.onUpdateRating(rating: value);
+                  },
+                ),
               ),
             ],
           ),
@@ -132,5 +144,47 @@ class CommentWriteScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  String ratingLabel({required double rate}) {
+    String rateLabel;
+    switch (rate) {
+      case 0:
+        rateLabel = '별점을 남겨주세요';
+        break;
+      case 0.5:
+        rateLabel = '그동안 즐거웠고 다시는 만나지 말자';
+        break;
+      case 1:
+        rateLabel = '아잇.. 너 참 싫다..';
+        break;
+      case 1.5:
+        rateLabel = '별로예요 :(';
+        break;
+      case 2:
+        rateLabel = '아쉬워요';
+        break;
+      case 2.5:
+        rateLabel = '약간 부족해요';
+        break;
+      case 3:
+        rateLabel = '볼만해요';
+        break;
+      case 3.5:
+        rateLabel = '꽤 재미있어요 ᵔ.ᵔ';
+        break;
+      case 4:
+        rateLabel = '이 작품을 추천해요!';
+        break;
+      case 4.5:
+        rateLabel = '취향 저격, 헤이! 츄라이! 츄라이!';
+        break;
+      case 5:
+        rateLabel = '더 이상 말이 필요없는 갓 띵작';
+        break;
+      default:
+        rateLabel = '별점을 남겨주세요';
+    }
+    return rateLabel;
   }
 }
